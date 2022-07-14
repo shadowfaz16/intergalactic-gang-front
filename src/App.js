c3ffec28fd07d7f67e445dcbaa6e29bc35b3cfcd
shadowfaz16 <erikfazju@gmail.com>
2022-07-14 02:07:46 -0500

    getting website started with tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+
+// Import Images + CSS
+import twitter from "./images/socials/twitter.svg";
+import instagram from "./images/socials/instagram.svg";
+import opensea from "./images/socials/opensea.svg";
+import showcase from "./images/showcase.png";
+import NFTs from "./images/NFTs.gif";
+import Hero from "./Components/Hero";
+import Distribution from "./Components/Distribution";
+import BuyAnAlien from "./Components/BuyAnAlien";
+import Specs from "./Components/Specs";
+import LaUniversidad from "./Components/LaUniversidad";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-black ">
+      <Navbar />
+      <Hero />
+      <Distribution />
+      <BuyAnAlien />
+      <Specs />
+      <LaUniversidad />
     </div>
   );
 }
